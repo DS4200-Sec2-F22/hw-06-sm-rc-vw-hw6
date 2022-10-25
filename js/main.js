d030b8032bd94c0f53607f17c1b25f807a279e1e
rachelcassway <105314113+rachelcassway@users.noreply.github.com>
2022-10-25 19:50:01 -0400

    left chart done, need brushing fill to work

--- js/main.js
+++ js/main.js
@@ -18,10 +18,9 @@ const FRAME1 =
 
 // read in the data
 d3.csv("data/iris.csv").then( function(data) {
-
     // add x-axis
     const xScale = d3.scaleLinear()
-        .domain([4, 8])
+        .domain([0, 8])
         .range([ 0, VIS_WIDTH]);
   	FRAME1.append("g")
     	.attr("transform", "translate(0," + 300 + ")")
@@ -29,7 +28,7 @@ d3.csv("data/iris.csv").then( function(data) {
 
 	// Add y-axis
 	const yScale = d3.scaleLinear()
-  		.domain([0, 9])
+  		.domain([0, 7])
   		.range([VIS_HEIGHT - 100, 0])
 	FRAME1.append("g")
   		.call(d3.axisLeft(yScale));
@@ -42,30 +41,43 @@ d3.csv("data/iris.csv").then( function(data) {
     // add points to scatter plot
     const myCircle = FRAME1.append('g')
     	.selectAll("dot")
-    	// .data(data)
-        .data(data.map(function(d) { return +d; }))
+    	.data(data)
+        // .data(data.map(function(d) { return +d; }))
     	.enter()
     	.append("circle")
-      	.attr("cx", function (d) { return xScale(d.Petal_Length); } )
-      	.attr("cy", function (d) { return yScale(d.Sepal_Length); } )
-      	.attr("r", 8)
-      	.attr('fill', function (d) {return (d.Species); } )
+      	.attr("cx", function (d) { return xScale(d.Sepal_Length); } )
+      	.attr("cy", function (d) { return yScale(d.Petal_Length); } )
+      	.attr("r", 5)
+      	.style("fill", function (d) { return color(d.Species) } )
+		.style("opacity", 0.5)
       	.attr('class', 'datapoint');
 
 
-     // add brushing feature
-    function updateGraph() {
-        extent = d3.event.selection
-        myCircle.classed("selected", function(d) {
-            return isBrushed(extent, x(d.Petal_Length), y(d.Sepal_Length) ) } )
+
+
+	// Add brushing
+  	FRAME1
+		// add brushing feature
+    	.call( d3.brush()
+		// initialize brush area
+      	.extent( [ [0,0], [VIS_WIDTH,VIS_HEIGHT] ] )
+		// when brush area selection changes, trigger updateChart function below
+      	.on("start brush", updateChart)
+    )
+
+	// function is triggered when brush area is updated
+	function updateChart() {
+	  	extent = d3.event.selection;
+	  	myCircle.classed("selected", function(d){ return isBrushed(extent, xScale(d.Sepal_Length), yScale(d.Petal_Length) ) } )
     }
 
-    // function that returns TRUE or FALSE depending on if point is in/not in the selection
-    function isBrushed(coords_brushed, cx, cy) {
-       var x0 = coords_brushed[0][0],
-           x1 = coords_brushed[1][0],
-           y0 = coords_brushed[0][1],
-           y1 = coords_brushed[1][1];
+  	// true or false, depending on selection pointsxw
+  	function isBrushed(brush_coords, cx, cy) {
+       const x0 = brush_coords[0][0],
+           x1 = brush_coords[1][0],
+           y0 = brush_coords[0][1],
+           y1 = brush_coords[1][1];
       return x0 <= cx && cx <= x1 && y0 <= cy && cy <= y1;
-    }
+  	}
+
 })
